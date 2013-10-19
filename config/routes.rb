c732a5493f0369768fb0c27e12ca35653150582f
Savannah Durbin <savannah.durbin@gmail.com>
2013-10-18 22:59:06 -0400

    Added bootstrap gem and root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Sportychickapparel::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
