3b70e4580d63abd1d1e4fdf531e83f62eb15ef93
Savannah Durbin <savannah.durbin@gmail.com>
2013-11-02 20:39:41 -0400

    created new gallery page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Sportychickapparel::Application.routes.draw do
   root 'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/gallery"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
