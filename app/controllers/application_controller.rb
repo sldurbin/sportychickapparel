09bec46dd7c7dfe198b918e1dfe8eb4cc09f8f33
Savannah Durbin <savannah.durbin@gmail.com>
2013-12-14 11:59:22 -0500

    Added social links on footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :get_instagram_feed, :setup_menu
+  before_filter :setup_menu
 
   def get_instagram_feed
     @instagram = Instagram.user_recent_media("631846379", {:count => 9})
